7ed139c218a2d0da01ef0b1e1ab1d64eb69e945e
Mohammad Hasan <mohd.hasan@mail.utoronto.ca>
2021-05-25 12:03:38 +0400

    updated portfolio, skills and certifications

--- js/script.js
+++ js/script.js
@@ -27,10 +27,34 @@ $(document).ready(function() {
                 items:4
             }
         }
-    })
+    });
 
-    $('.chart').easyPieChart({
-            //your options goes here
+
+    $('.items').isotope({
+        filter: '*',
+        animationOptions: {
+            duration: 1500,
+            easing: 'linear',
+            queue: false
+        }
+    });
+
+    $("#filters a").click(function() {
+
+        $("#filters .current").removeClass("current");
+        $(this).addClass("current");
+
+        var selector = $(this).attr("data-filter");
+        $('.items').isotope({
+            filter: selector,
+            animationOptions: {
+                duration: 1500,
+                easing: 'linear',
+                queue: false
+            }
         });
+
+        return false;
+    });
 
 });
