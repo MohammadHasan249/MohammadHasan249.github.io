2989e8c8fbf943c58279cc0df67ae1e9c5ee2bf6
Mohammad Hasan <mohd.hasan@mail.utoronto.ca>
2021-05-25 12:43:45 +0400

    fixed carousel issue

--- js/script.js
+++ js/script.js
@@ -14,8 +14,11 @@ $(document).ready(function() {
 
   $('.owl-carousel').owlCarousel({
         loop:true,
-        margin:10,
-        nav:true,
+        margin:50,
+        responsiveClass:true,
+        autoplay:true,
+        autoplayTimeout:2000,
+        autoplayHoverPause:true,
         responsive:{
             0:{
                 items:1
