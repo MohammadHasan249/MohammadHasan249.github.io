4b815c957a5ca647a97ed3c0ce1adf922b579a25
Mohammad Hasan <mohd.hasan@mail.utoronto.ca>
2022-08-09 17:00:59 -0400

    Updated website

--- js/script.js
+++ js/script.js
@@ -25,7 +25,7 @@ $(document).ready(function() {
   });
 
   var typed = new Typed('.typed', {
-    strings: ['Software Engineer', 'Full-Stack Developer', 'Game Developer', 'Data Scientist', 'Student'],
+    strings: ['Software Engineer', 'Full-Stack Developer', 'Game Developer', 'Data Scientist'],
     typeSpeed: 70,
     loop: true,
     startDelay: 1000
